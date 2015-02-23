a0ee2f28fab7e9822e32eab2cbb9fdc9736bfe43
Ward Schodts <wardschodts@hotmail.com>
2015-02-23 23:18:52 +0100

    Documentatie fatsoenlijk gemaakt van UseCase1. Nieuwe test toegevoegd die een project finishen test.

--- src/be/swop/groep11/main/Project.java
+++ src/be/swop/groep11/main/Project.java
@@ -1,7 +1,15 @@
 package be.swop.groep11.main;
 
+import java.time.LocalDateTime;
+
 /**
  * Created by warreee on 23/02/15.
  */
 public class Project {
+    public ProjectStatus getStatus() {
+        return null;
+    }
+
+    public void addTask(String name, String description, double acceptableDeviation, LocalDateTime startTime, LocalDateTime endTime) {
+    }
 }
--- src/be/swop/groep11/test/UseCase1.java
+++ src/be/swop/groep11/test/UseCase1.java
@@ -16,19 +16,20 @@ public class UseCase1 {
     }
 
 
-    /*
-    Indien er geen projecten zijn toegevoegd, dan verwachten we een lege lijst.
+    /**
+    * Indien er geen projecten zijn toegevoegd, dan verwachten we een lege lijst.
      */
     @Test
     public void noProjectsTest() throws Exception {
         assertTrue("Geen projecten in Tman: ", projectManager.getProjects().isEmpty());
     }
 
-    /*
-    We voegen 4 projecten toe en kijken na dat:
-    - Er 4 projecten bestaan
-    Hierna voegen we er nog een toe en kijken na dat:
-    - Er 5 projecten bestaan
+    /**
+    *  We voegen 4 projecten toe en kijken na dat:
+    * - Er 4 projecten bestaan
+    * Hierna voegen we er nog een toe en kijken na dat:
+    * - Er 5 projecten bestaan
+    * Hierna testen we of de stati van deze 5 projecten overeenkomen met ONGOING aangezien dit de standaard status is.
      */
 
     @Test
@@ -36,12 +37,37 @@ public class UseCase1 {
         addProjectsToProjectManager(4);
         assertTrue("4 projecten in Tman: ", projectManager.getProjects().size() == 4);
         addProjectsToProjectManager(1);
-        assertTrue("5 projecten in Tman: ", projectManager.getProjects().size() == 5);
+        assertTrue("5 projects in Tman: ", projectManager.getProjects().size() == 5);
+        for(Project p: projectManager.getProjects()){
+            assertTrue("Projectstatus is ongoing: ", p.getStatus().equals(ProjectStatus.ONGOING));
+        }
     }
 
+    /**
+    * We voegen een project toe, daarna een taak aan dit project.
+    * Hierna finishen we de taak.
+    * We finishen het project.
+    * We kijken na of het project effectief gefinished is.
+     */
+    @Test
+    public void projectFinishedTest() throws Exception {
+        addProjectsToProjectManager(1);
+        Project testProject = projectManager.getProjects().get(0);
+        addTasksToProject(testProject, 1);
+        Task testTask = testProject.getTasks().get(0);
+        testTask.finish();
+        assertFalse("Project status is not finished: ", testProject.getStatus().equals(ProjectStatus.FINISHED));
+        testProject.finish();
+        assertTrue("Project status is finished: ", testProject.getStatus().equals(ProjectStatus.FINISHED));
+    }
+
+
+
     /////////////////////////////////////////HELPER FUNCTIONS///////////////////////////////////////////////////////////
-    /*
-    Deze methode voeg numberOfProjects aantal projecten toe aan projectManager
+
+    /**
+     * Deze methode voegt numberOfProjects aantal projecten toe aan projectManager.
+     * @param numberOfProjects Het aantal toe te voegen projecten.
      */
     private void addProjectsToProjectManager(long numberOfProjects){
 
@@ -52,6 +78,22 @@ public class UseCase1 {
             LocalDateTime duetime = creationTime.plusDays(2);
             User user = new User("Test User");
             projectManager.addProject(name, description, creationTime, duetime, user);
+        }
+    }
+
+    /**
+     * Voegt numberOfTasks Tasks toe aan een gegeven project.
+     * @param project Het project waaraan taken moeten worden toegevoegd.
+     * @param numberOfTasks Het aantal toe te voegen taken.
+     */
+    private void addTasksToProject(Project project, long numberOfTasks){
+        for(int i=0; i<numberOfTasks; i++){
+            String name = "Test task";
+            String description = "Test description.";
+            double acceptableDeviation = 0.05;
+            LocalDateTime startTime = LocalDateTime.now();
+            LocalDateTime endTime = LocalDateTime.now();
+            project.addTask(name, description, acceptableDeviation, startTime, endTime);
         }
     }
 }
