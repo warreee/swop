141b4feeecddce8469621e43cba8f8fe3d1ecb71
arnedb <arne.debrabandere@student.kuleuven.be>
2015-03-02 16:45:03 +0100

    Implementatie DependencyConstraint en Task

--- src/be/swop/groep11/main/DependencyConstraint.java
+++ src/be/swop/groep11/main/DependencyConstraint.java
@@ -2,6 +2,8 @@ package be.swop.groep11.main;
 
 import com.google.common.collect.ImmutableList;
 
+import java.util.Set;
+
 /**
  * Stelt de afhankelijkheid van een taak voor.
  */
@@ -14,12 +16,12 @@ public class DependencyConstraint {
 
     /**
      * Maakt een nieuwe dependency constraint aan.
-     * @param dependingOn De taak waarvan dependentTask afhankelijk moet zijn
-     * @param dependentTask De afhankelijke taak
+     * @param dependingOn De taak waarvan de gegeven taak (task) afhankelijk moet zijn
+     * @param task De gegeven taak
      * @throws java.lang.IllegalArgumentException Ongeldige dependency constraint
      */
-    public DependencyConstraint(Task dependingOn, Task dependentTask) throws IllegalArgumentException {
-        if (! isValidDependingOn(dependingOn,dependentTask))
+    public DependencyConstraint(Task dependingOn, Task task) throws IllegalArgumentException {
+        if (! isValidDependingOn(dependingOn,task))
             throw new IllegalArgumentException("Invalid depedency constraint");
         this.dependingOn = dependingOn;
     }
@@ -32,14 +34,14 @@ public class DependencyConstraint {
     }
 
     /**
-     * Controleer of een taak (dependingOn) een geldige taak is voor een gegeven afhankelijke taak (dependentTask)
-     * @param dependingOn De taak waarvan gecontroleerd moet worden of dependentTask daarvan afhankelijk moet zijn
-     * @param dependentTask De afhankelijke taak
-     * @return true als en slechts als een taak een geldige dependingOn taak is voor een bepaalde afhankelijke taak,
+     * Controleer of een taak (dependingOn) een geldige taak is voor een gegeven taak (task)
+     * @param dependingOn De taak waarvan gecontroleerd moet worden of task daarvan afhankelijk moet zijn
+     * @param task De gegeven taak
+     * @return true als en slechts als een taak een geldige dependingOn taak is voor de gegeven taak,
      * (als en slechts als het toevoegen van de dependency constraint geen lussen veroorzaakt in de "dependency graph")
      */
-    public boolean isValidDependingOn(Task dependingOn, Task dependentTask) {
-        ImmutableList<Task> dependentTasks = dependentTask.getDependentTasks();
+    public boolean isValidDependingOn(Task dependingOn, Task task) {
+        Set<Task> dependentTasks = task.getDependentTasks();
         if (dependentTasks.contains(dependingOn))
             return false;
         return true;
