11291aab9a76167b7c23de2c4383172b01b77ce6
arnedb <arne.debrabandere@student.kuleuven.be>
2015-03-07 20:38:46 +0100

    Wijzigingen aan isValidNewStatus()

--- src/be/swop/groep11/main/TaskStatus.java
+++ src/be/swop/groep11/main/TaskStatus.java
@@ -11,18 +11,39 @@ public enum TaskStatus {
     FAILED;
 
     /**
-     * Controleert of een status een geldige nieuwe status is voor een taak
+     * Controleert of een status een geldige nieuwe status is voor een taak.
+     * De geldige overgangen zijn:
+     *
+     *      AVAILABLE -> AVAILABLE
+     *      AVAILABLE -> UNAVAILABLE
+     *      AVAILABLE -> FINISHED/FAILED indien taak een start- en eindtijd heeft
+     *
+     *      UNAVAILABLE -> UNAVAILABLE
+     *      UNAVAILABLE -> AVAILABLE indien alle taken waarvan task afhankelijk is beëindigd zijn
+     *
+     *      FINISHED -> FINISHED
+     *
+     *      FAILED -> FAILED
+     *
      * @param status De nieuwe status om te checken
      * @param task De taak
      * @return true alss status een geldige nieuwe status is voor een taak
      */
-    public static boolean canChangeStatus(TaskStatus status, Task task) {
+    public static boolean isValidNewStatus(TaskStatus status, Task task) {
         TaskStatus currentStatus = task.getStatus();
         switch (currentStatus) {
             case AVAILABLE:
+                if (status == FINISHED || status == FAILED)
+                    return task.getStartTime() != null && task.getEndTime() != null;
                 return true;
             case UNAVAILABLE:
-                return status == AVAILABLE || status == UNAVAILABLE;
+                if (status == AVAILABLE) {
+                    for (Task dependingOn : task.getDependingOnTasks())
+                        if (task.getStatus() != FINISHED)
+                            return false;
+                    return true;
+                }
+                return status == UNAVAILABLE;
             case FINISHED:
                 return status == FINISHED;
             case FAILED:
