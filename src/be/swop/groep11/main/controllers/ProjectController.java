4c7667a10cf632a9ff64fddb1512f527c7d5d3c2
warreee <wardschodts@hotmail.com>
2015-03-09 17:35:40 +0100

    Controllers added

--- src/be/swop/groep11/main/controllers/ProjectController.java
+++ src/be/swop/groep11/main/controllers/ProjectController.java
@@ -1,7 +1,31 @@
 package be.swop.groep11.main.controllers;
 
+import be.swop.groep11.main.Project;
+import be.swop.groep11.main.ProjectRepository;
+import be.swop.groep11.main.ProjectStatus;
+import com.google.common.collect.ImmutableList;
+
 /**
  * Created by warreee on 3/9/15.
  */
 public class ProjectController {
+
+    public ProjectController(ProjectRepository projectRepository){
+        this.projectRepository = projectRepository;
+    }
+
+    ProjectRepository projectRepository;
+
+    /**
+     * Geeft een immutable lijst terug van alle projecten
+     */
+    public ImmutableList<Project> getAllProjects(){
+        return projectRepository.getProjects();
+    }
+
+    public ProjectStatus getProjectStatus(Project project){
+        return project.getProjectStatus();
+    }
+
+
 }
--- src/be/swop/groep11/main/controllers/TaskController.java
+++ src/be/swop/groep11/main/controllers/TaskController.java
@@ -4,4 +4,7 @@ package be.swop.groep11.main.controllers;
  * Created by warreee on 3/9/15.
  */
 public class TaskController {
+
+
+
 }
