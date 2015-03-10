dff33bb0aa1432daa60b4061231d91b3df0cdf65
Ward Schodts <wardschodts@hotmail.com>
2015-03-10 23:22:39 +0100

    TaskController aangevuld

--- src/be/swop/groep11/main/controllers/TaskController.java
+++ src/be/swop/groep11/main/controllers/TaskController.java
@@ -1,10 +1,35 @@
 package be.swop.groep11.main.controllers;
 
+import be.swop.groep11.main.Project;
+import be.swop.groep11.main.Task;
+import be.swop.groep11.main.TaskStatus;
+import be.swop.groep11.main.ui.UserInterface;
+import com.google.common.collect.ImmutableList;
+
+import java.time.Duration;
+
 /**
  * Created by warreee on 3/9/15.
  */
 public class TaskController {
 
+    Project project;
+    UserInterface ui;
 
+    public TaskController(Project project, UserInterface ui){
+        this.project = project;
+        this.ui = ui;
+    }
 
+    public void createTask(String description, int acceptableDeviation, Duration estimatedDuration){
+        project.addNewTask(description, acceptableDeviation, estimatedDuration);
+    }
+
+    public ImmutableList<Task> getAllTasks(){
+        return project.getTasks();
+    }
+
+    public void updateStatus(Task task, TaskStatus status){
+        task.setStatus(status);
+    }
 }
--- src/be/swop/groep11/main/ui/CommandLineInterface.java
+++ src/be/swop/groep11/main/ui/CommandLineInterface.java
@@ -13,7 +13,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Map;
-import java.util.Scanner;
 
 /**
  * Commandline gebruikersinterface die UserInterface implementeert.
