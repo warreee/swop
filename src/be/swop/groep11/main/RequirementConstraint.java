b7783aee1e64d7fbcf146df3ff140da662972a4e
Robin Goots <robin5210@ulyssis.org>
2015-04-05 23:07:38 +0200

    Constraint klassen toegevoegd.

---
+++ src/be/swop/groep11/main/RequirementConstraint.java
@@ -0,0 +1,7 @@
+package be.swop.groep11.main;
+
+/**
+ * Created by robin on 5/04/15.
+ */
+public class RequirementConstraint extends ResourceTypeConstraint {
+}
---
+++ src/be/swop/groep11/main/ResourceTypeConstraint.java
@@ -0,0 +1,8 @@
+package be.swop.groep11.main;
+
+/**
+ * Created by robin on 5/04/15.
+ */
+public abstract class ResourceTypeConstraint {
+
+}
