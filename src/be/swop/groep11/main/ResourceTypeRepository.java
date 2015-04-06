fda321c94f2339dbc67a2bc1494a3fb8e221b98d
Robin Goots <robin5210@ulyssis.org>
2015-04-06 14:19:11 +0200

    ResourceTypeRepository kan nu ResourceTypes bijhouden.

--- src/be/swop/groep11/main/ResourceTypeRepository.java
+++ src/be/swop/groep11/main/ResourceTypeRepository.java
@@ -1,7 +1,32 @@
 package be.swop.groep11.main;
 
+import com.google.common.collect.ImmutableList;
+
+import java.util.ArrayList;
+
 /**
  * Created by robin on 2/04/15.
  */
 public class ResourceTypeRepository {
+
+    public ResourceTypeRepository(){
+
+    }
+
+    private ArrayList<ResourceType> resourceTypes = new ArrayList<ResourceType>();
+
+    public ImmutableList<ResourceType> getResourceTypes() {
+        return ImmutableList.copyOf(resourceTypes);
+    }
+
+    public void addResourceType(ResourceType type) {
+        if (!isValidResourceType(type)){
+            throw new IllegalArgumentException("Geen geldig RescourceType aan ResourceTypeRepository toegevoegd.");
+        }
+        resourceTypes.add(type);
+    }
+
+    public static boolean isValidResourceType(ResourceType type){
+        return type != null;
+    }
 }
