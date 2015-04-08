03e722cfa79fa49b63306418d6b9da488c8513e6
arnedb <arne.debrabandere@student.kuleuven.be>
2015-04-08 13:53:58 +0200

    Developer: isAvailable() en getNextAvailableTimeSpan()

--- src/be/swop/groep11/main/Developer.java
+++ src/be/swop/groep11/main/Developer.java
@@ -5,6 +5,7 @@ import com.google.common.collect.ImmutableList;
 import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -25,15 +26,123 @@ public class Developer extends User implements ResourceInstance {
         this.resourceType = resourceType;
     }
 
+    /**
+     * Controleert of deze developer beschikbaar is gedurende een gegeven tijdsspanne.
+     * @param timeSpan De gegeven tijdsspanne
+     * @return True als deze developer beschikbaar is, rekening houdend met dat de developer al "gereserveerd" kan zijn.
+     */
     @Override
     public boolean isAvailable(TimeSpan timeSpan) {
-        return false; // TODO
+        return this.getConflictingAllocations(timeSpan).isEmpty();
     }
 
+    private List<ResourceAllocation> getConflictingAllocations(TimeSpan timeSpan) {
+        List<ResourceAllocation> conflictingAllocations = new ArrayList<>();
+        ImmutableList<ResourceAllocation> allocations = this.getAllocations();
+        for (ResourceAllocation allocation : allocations) {
+            if (timeSpan.overlapsWith(allocation.getTimeSpan())) {
+                conflictingAllocations.add(allocation);
+            }
+        }
+        return conflictingAllocations;
+    }
+
+    /**
+     * Geeft de eerst volgende tijdsspanne waarin deze resource voor een gegeven duur beschikbaar is,
+     * na een gegeven starttijd.
+     * Hierbij wordt rekening gehouden dat een developer van 8u tot 17u werkt en een middagpauze van 1u moet
+     * hebben tussen 11u en 14u.
+     * en er al allocaties kunnen zijn.
+     * @param startTime De gegeven starttijd
+     * @param duration  De gegeven duur
+     */
     @Override
     public TimeSpan getNextAvailableTimeSpan(LocalDateTime startTime, Duration duration) {
-        return null; // TODO
+        // de "echte" starttijd is dan het eerste moment dat binnen de dagelijkse beschikbaarheid ligt
+        LocalDateTime realStartTime = this.getDailyAvailability().getNextTime(startTime);
+
+        LocalDateTime realEndTime = calculateEndTime(realStartTime, duration);
+        TimeSpan timeSpan = new TimeSpan(realStartTime, realEndTime);
+
+        if (! this.isAvailable(timeSpan)) {
+            List<ResourceAllocation> conflictingAllocations = this.getConflictingAllocations(timeSpan);
+            // bereken hiermee de volgende mogelijke starttijd = de grootste van alle eindtijden van de resources
+            LocalDateTime nextStartTime = realStartTime;
+            for (ResourceAllocation allocation : conflictingAllocations) {
+                if (allocation.getTimeSpan().getEndTime().isAfter(nextStartTime)) {
+                    nextStartTime = allocation.getTimeSpan().getEndTime();
+                }
+            }
+
+            return getNextAvailableTimeSpan(nextStartTime, duration);
+        }
+
+        return timeSpan;
     }
+
+    private LocalDateTime calculateEndTime(LocalDateTime startTime, Duration duration) {
+        LocalDateTime currentStartTime = startTime;
+        LocalDateTime currentEndTime   = null;
+        Duration      currentDuration  = duration;
+
+        while (!currentDuration.isZero()) {
+            Duration durationUntilNextEndTime = this.getDailyAvailability().getDurationUntilNextEndTime(currentStartTime);
+            if (currentDuration.compareTo(durationUntilNextEndTime) <= 0) {
+                currentEndTime = currentStartTime.plus(currentDuration);
+                currentDuration = Duration.ZERO;
+            }
+            else {
+                currentEndTime = currentStartTime.plus(durationUntilNextEndTime);
+                currentDuration = currentDuration.minus(durationUntilNextEndTime);
+                currentStartTime = this.getDailyAvailability().getNextStartTime(currentEndTime);
+            }
+
+            // rekening houden met de middagpauze: middagpauze opnemen in de duur van de reservatie
+            // verschillende gevallen: zie uml/Developer.calculateEndTime - verschillende gevallen.jpg
+            boolean addBreak;
+            if (currentEndTime.toLocalTime().isAfter(endOfBreak.minus(breakDuration)) && currentStartTime.toLocalTime().isBefore(startOfBreak.plus(breakDuration))) {
+                /* GEVAL 3 en 4: voeg een middagpauze toe als [currentStartTime] < [endOfBreak]+[breakDuration] & [currentEndTime] > [endOfBreak]-[breakDuration] */
+                addBreak = true;
+            }
+            else if (! currentEndTime.toLocalTime().isAfter(endOfBreak.minus(breakDuration))) {
+                /* GEVAL 2: voeg geen middagpauze toe */
+                addBreak = false;
+            }
+            else {
+                /* GEVAL 1 */
+                if (! currentEndTime.toLocalTime().isBefore(startOfBreak)) {
+                    /* voeg een middagpauze toe op dezelfde dag als [currentEndTime] >= [startOfBreak] */
+                    addBreak = true;
+                }
+                else {
+                    addBreak = false;
+                }
+            }
+
+            if (addBreak) {
+                // voeg een middagpauze toe
+                currentDuration = currentDuration.plus(breakDuration);
+                durationUntilNextEndTime = this.getDailyAvailability().getDurationUntilNextEndTime(currentEndTime);
+                if (currentDuration.compareTo(durationUntilNextEndTime) <= 0) {
+                    /* voeg een middagpauze toe op dezelfde dag als dat kan*/
+                    currentEndTime = currentEndTime.plus(breakDuration);
+                    currentDuration = Duration.ZERO;
+                }
+                else {
+                    /* voeg een middagpauze toe, (deels) op een andere dag */
+                    currentEndTime = currentStartTime.plus(durationUntilNextEndTime);
+                    currentDuration = currentDuration.minus(durationUntilNextEndTime);
+                    currentStartTime = this.getDailyAvailability().getNextStartTime(currentEndTime);
+                }
+            }
+        }
+
+        return currentEndTime;
+    }
+
+    private static LocalTime startOfBreak  = LocalTime.of(11,0);
+    private static LocalTime endOfBreak    = LocalTime.of(14,0);
+    private static Duration  breakDuration = Duration.ofHours(1);
 
     @Override
     public ResourceType getResourceType() {
