6321d2eb72b35f3139c088662e2ed3a8139b87a1
warreee <wardschodts@hotmail.com>
2015-04-08 15:56:41 +0200

    TaskFinished geïmplementeerd

--- src/be/swop/groep11/main/task/TaskFinished.java
+++ src/be/swop/groep11/main/task/TaskFinished.java
@@ -10,26 +10,26 @@ public class TaskFinished extends TaskStatus2 {
 
     @Override
     public void execute(Task task) {
-
+        throw new IllegalStateTransition("Een gefinishste taak kan niet opnieuw worden uitgevoerd");
     }
 
     @Override
     public void finish(Task task) {
-
+        throw new IllegalStateTransition("De taak was reeds gefinishd");
     }
 
     @Override
     public void fail(Task task) {
-
+        throw new IllegalStateTransition("Een gefinishste taak kan niet gefaild worden!");
     }
 
     @Override
     public void makeAvailable(Task task) {
-
+        throw new IllegalStateTransition("Een gefinishste taak kan niet available worden!");
     }
 
     @Override
     public void makeUnavailable(Task task) {
-
+        throw new IllegalStateTransition("Een gefinishste taak kan niet unavailable worden!");
     }
 }
