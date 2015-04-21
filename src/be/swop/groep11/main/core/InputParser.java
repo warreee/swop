d6ece96099178e703755fe9d6a34514060825b8a
Robin Goots <robin5210@ulyssis.org>
2015-04-21 09:23:47 +0200

    Merge enz en wat gewerkt aan InputParser

--- src/be/swop/groep11/main/core/InputParser.java
+++ src/be/swop/groep11/main/core/InputParser.java
@@ -28,32 +28,28 @@ public class InputParser {
 
     User user = new User("InputParser");
     ProjectRepository projectRepository;
-    ImmutableList<Project> projectList;
+    ArrayList<Project> projectList = new ArrayList<>();
     ArrayList<Task> taskList = new ArrayList<>();
     private ResourceTypeRepository typeRepo;
 
     public InputParser(ProjectRepository projectRepository, ResourceTypeRepository typeRepo) {
         this.projectRepository = projectRepository;
         this.typeRepo = typeRepo;
-        this.projectList = this.projectRepository.getProjects();
     }
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         TMSystem tmSystem = new TMSystem();
-//        ProjectRepository projectRepository = new ProjectRepository(tmSystem);
-/*        InputParser inputParser = new InputParser(projectRepository, );
-        try {
-            inputParser.parseInputFile();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }*/
+        ProjectRepository projectRepository = new ProjectRepository(tmSystem);
+        ResourceTypeRepository typeRepo = new ResourceTypeRepository();
+        InputParser parser = new InputParser(projectRepository, typeRepo);
+        parser.parseInputFile();
     }
-
 
 
     /**
      * Leest input.tman in, parset de file en maakt de objecten aan in de meegegeven projectRepository
+     *
      * @throws FileNotFoundException indien het input.tman niet op de juiste plaats staat.
      */
     public void parseInputFile() throws FileNotFoundException {
@@ -61,19 +57,24 @@ public class InputParser {
         Yaml yaml = new Yaml();
         String path = Paths.get("input/input.tman").toAbsolutePath().toString();
 
-        Map<String, Map<String, String>> values = null;
+        Map<String, Object> values;
 
         try {
             values = (Map<String, Map<String, String>>) yaml
                     .load(new FileInputStream(new File(path)));
         } catch (FileNotFoundException e) {
             System.out.println(e.getMessage());
+            return; // Return omdat de inputfile niet gelezen kon worden.
         }
+
+        handleProjects(values);
+
+
         // Voegt eerst de projecten toe, daarna de taken.
         for (String key : values.keySet()) {
             ArrayList subList;
 
-            if (key.equals("projects")){
+            if (key.equals("projects")) {
                 subList = (ArrayList) values.get(key);
                 for (int i = 0; i < subList.size(); i++) {
                     Map<String, String> mapProject = (Map<String, String>) subList.get(i);
@@ -81,7 +82,7 @@ public class InputParser {
                 }
             }
 
-            if (key.equals("tasks")){
+            if (key.equals("tasks")) {
                 subList = (ArrayList) values.get(key);
                 for (int i = 0; i < subList.size(); i++) {
                     Map<String, String> mapTask = (Map<String, String>) subList.get(i);
@@ -93,19 +94,19 @@ public class InputParser {
                 }
             }
 
-            if (key.equals("systemTime")){
+            if (key.equals("systemTime")) {
                 String sysTime = String.valueOf(values.get(key));
                 LocalDateTime sytemTime = parseTime(sysTime);
             }
 
-            if (key.equals("dailyAvailability")){
+            if (key.equals("dailyAvailability")) {
                 subList = (ArrayList) values.get(key);
                 for (int i = 0; i < subList.size(); i++) {
                     Map<String, String> mapAvailability = (Map<String, String>) subList.get(i);
                 }
             }
 
-            if (key.equals("resourceTypes")){
+            if (key.equals("resourceTypes")) {
                 subList = (ArrayList) values.get(key);
                 for (int i = 0; i < subList.size(); i++) {
                     Map<String, String> mapResourceTypes = (Map<String, String>) subList.get(i);
@@ -113,28 +114,28 @@ public class InputParser {
             }
 
 
-            if (key.equals("resources")){
+            if (key.equals("resources")) {
                 subList = (ArrayList) values.get(key);
                 for (int i = 0; i < subList.size(); i++) {
                     Map<String, String> mapResources = (Map<String, String>) subList.get(i);
                 }
             }
 
-            if (key.equals("developers")){
+            if (key.equals("developers")) {
                 subList = (ArrayList) values.get(key);
                 for (int i = 0; i < subList.size(); i++) {
                     Map<String, String> mapDevelopers = (Map<String, String>) subList.get(i);
                 }
             }
 
-            if (key.equals("plannings")){
+            if (key.equals("plannings")) {
                 subList = (ArrayList) values.get(key);
                 for (int i = 0; i < subList.size(); i++) {
                     Map<String, String> mapPlannings = (Map<String, String>) subList.get(i);
                 }
             }
 
-            if (key.equals("reservations")){
+            if (key.equals("reservations")) {
                 subList = (ArrayList) values.get(key);
                 for (int i = 0; i < subList.size(); i++) {
                     Map<String, String> mapReservations = (Map<String, String>) subList.get(i);
@@ -143,6 +144,21 @@ public class InputParser {
 
         }
 
+    }
+
+    private void handleProjects(Map<String, Object> projectsMap){
+        ArrayList projects = (ArrayList) projectsMap.get("projects");
+        for(Object project: projects){
+            addProject((Map<String, String>) project);
+        }
+    }
+
+    private void handleTasks(Map<String, Object> tasksMap){
+        ArrayList tasks = (ArrayList) tasksMap.get("tasks");
+        for(Object task: tasks){
+            Map<String, String> taskMap = (Map<String, String>) task;
+            int projectIndex = Integer.valueOf(String.valueOf(taskMap.get("project")));
+        }
     }
 
 //    private void addType(Map<String, String> typeMap){
@@ -156,7 +172,7 @@ public class InputParser {
     /**
      * Init IResourceTypes a.d.h.v. hun namen.
      */
-    private void withTypes(List<String> types) throws IllegalArgumentException{
+    private void withTypes(List<String> types) throws IllegalArgumentException {
         if (types == null || types.isEmpty()) {
             throw new IllegalArgumentException("Eerst types bepalen met");
         }
@@ -173,12 +189,12 @@ public class InputParser {
         }
         IResourceType ownerType = typeRepo.getResourceTypeByName(typeName);
 
-        typeRepo.withDailyAvailability(ownerType,availability);
+        typeRepo.withDailyAvailability(ownerType, availability);
 
         //add require constraints
         for (String req : requires) {
             IResourceType reqType = typeRepo.getResourceTypeByName(req);
-            typeRepo.withRequirementConstraint(ownerType,reqType);
+            typeRepo.withRequirementConstraint(ownerType, reqType);
         }
         //Add conflicting constraints
         for (String con : conflicts) {
@@ -188,14 +204,15 @@ public class InputParser {
 
         //Add instances
         for (String inst : instances) {
-            typeRepo.addResourceInstance(ownerType,inst);
+            typeRepo.addResourceInstance(ownerType, inst);
         }
     }
 
     /**
      * addOtherDetails voegt details toe aan de taken die niet worden gezet bij de constructie van een taak in project.
+     *
      * @param mapTask de ingelezen details die moeten worden toegevoegd.
-     * @param task de taak waar de details aan worden toegevoegd.
+     * @param task    de taak waar de details aan worden toegevoegd.
      */
     private void addOtherDetails(Map<String, String> mapTask, Task task) {
 
@@ -247,12 +264,13 @@ public class InputParser {
 
     /**
      * Zorgt voor de parsing van een string naar een TaskStatus
+     *
      * @param strStatus de status in stringvorm
      * @return de taskstatus
      * @throws IllegalArgumentException indien er string wordt meegegeven waar geen status mee overeenkomt
-     *          (is niet hoofdlettergevoelgi)
+     *                                  (is niet hoofdlettergevoelgi)
      */
-    private TaskStatus stringToStatus(String strStatus) throws IllegalArgumentException{
+    private TaskStatus stringToStatus(String strStatus) throws IllegalArgumentException {
 
         TaskStatus result = null;
 
@@ -271,10 +289,11 @@ public class InputParser {
 
     /**
      * Zet een string tijd om in een LocalDateTime object
+     *
      * @param date, in stringvorm
      * @return de LocalDateTime
      */
-    private LocalDateTime parseTime(String date){
+    private LocalDateTime parseTime(String date) {
         DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
         try {
             return LocalDateTime.parse(date, dateTimeFormatter);
@@ -287,6 +306,7 @@ public class InputParser {
 
     /**
      * Gebruikt de projectrepostory om een project toe te voegen aan het systeem
+     *
      * @param propertiesList de eigenschappen van het project ingelezen vanuit de tman file.
      */
     private void addProject(Map<String, String> propertiesList) {
@@ -298,12 +318,59 @@ public class InputParser {
 
         projectRepository.addNewProject(name, description, creationTime, dueTime, user);
 
+        // Haalt het laatst toegevoegde project op.
+        projectList.add(projectRepository.getProjects().get(projectRepository.getProjects().size() - 1 ));
+    }
+
+    private void addTask(Map<String, String> propertiesList) {
+
+        // TODO: planning er in steken.
+        // TODO: Status nog eens goed controlleren
+
+        String description = propertiesList.get("description");
+        Duration duration = Duration.ofMinutes(Long.valueOf(String.valueOf(propertiesList.get("estimatedDuration"))));
+        Double acceptableDeviation = Double.valueOf(String.valueOf(propertiesList.get("acceptableDeviation"))) / 100;
+        Project project = projectList.get(Integer.valueOf(propertiesList.get("project")));
+        project.addNewTask(description, acceptableDeviation, duration);
+        taskList.add(project.getTasks().get(project.getTasks().size() - 1));
+        Task task = taskList.get(taskList.size() - 1);
+
+        if(propertiesList.containsKey("startTime")){
+            task.execute(parseTime(propertiesList.get("startTime")));
+        }
+
+        if(propertiesList.containsKey("endTime")){
+            if(propertiesList.get("status").equals("finished")) {
+                task.finish(parseTime(propertiesList.get("endTime")));
+            } else if(propertiesList.get("status").equals("failed")){
+                task.fail(parseTime(propertiesList.get("endTime")));
+            } else {
+                throw new IllegalArgumentException("Onbekende status bij het zetten van een eindtijd.");
+            }
+
+        }
+
+        if(propertiesList.containsKey("alternativeFor")){
+            try {
+                taskList.get(Integer.valueOf(propertiesList.get("alternativeFor"))).setAlternativeTask(task);
+            } catch (NumberFormatException e){
+                // Doe niks, de sleutel alternativeFor was waarschijnlijk leeg.
+            }
+        }
+
+        if(propertiesList.containsKey("prerequisiteTasks")){
+            for(int i: parseStringArray(propertiesList.get("prerequisiteTasks"))){
+                taskList.get(i).addNewDependencyConstraint(task);
+            }
+        }
+
     }
 
     /**
      * Gebruikt een aangemaakt project om een taak aan toe te voegen
+     *
      * @param propertiesList de eigenschappen van de taak in tman formaat
-     * @param project het project waar de taak aan wordt toegevoegd.
+     * @param project        het project waar de taak aan wordt toegevoegd.
      */
     private void addTaskToProject(Map<String, String> propertiesList, Project project) {
 
@@ -317,6 +384,7 @@ public class InputParser {
 
     /**
      * Maakt van een array in stringformaat een int[]
+     *
      * @param array in stringformaat
      * @return een array van ints
      */
@@ -331,7 +399,4 @@ public class InputParser {
         }
         return intArray;
     }
-
-
-
 }
