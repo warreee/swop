83350011277aa0c45a6d150c452337dfc8e6a4f4
warreee <wardschodts@hotmail.com>
2015-04-23 15:20:27 +0200

    Testing depencyGraph

--- tests/be/swop/groep11/test/unit/DependencyGraphTest.java
+++ tests/be/swop/groep11/test/unit/DependencyGraphTest.java
@@ -7,7 +7,14 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.function.Function;
+import java.util.stream.Stream;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 /**
@@ -19,6 +26,7 @@ public class DependencyGraphTest {
     SystemTime systemTime = new SystemTime();
     Double deviation = 0.2;
     DependencyGraph dependencyGraph = new DependencyGraph();
+    ArrayList<Task> dependencies;
     Task taskA;
     Task taskB;
     Task taskC;
@@ -38,8 +46,24 @@ public class DependencyGraphTest {
     @Test
     public void addDependencyTest(){
         dependencyGraph.addDependency(taskA, taskB);
-        assertTrue(true);
+        assertTrue(taskA.getDependingOnTasks().contains(taskB));
+        assertFalse(taskB.getDependingOnTasks().contains(taskA));
+        assertFalse(taskB.getDependingOnTasks().contains(taskB));
+        assertFalse(taskA.getDependingOnTasks().contains(taskA));
+
+        assertFalse(taskA.getDependentTasks().contains(taskB));
+        assertTrue(taskB.getDependentTasks().contains(taskA));
+        Set<Task> te = dependencyGraph.getDependentTasks(taskA);
+        System.out.println("test");
+        dependencyGraph.getDependentTasks(taskA).stream().map((t) -> t.getDescription() + "\n").forEach(System.out::println);
+        dependencyGraph.getDependentTasks(taskB).stream().map((t) -> t.getDescription() + "\n").forEach(System.out::println);
     }
+
+
+
+
+
+
 
 }
 
