db8f398781d9133ee07498df09916a65017beacf
Robin Goots <robin5210@ulyssis.org>
2015-05-01 14:22:12 +0200

    Documentatie in TaskExecuting.java

--- src/be/swop/groep11/main/task/TaskExecuting.java
+++ src/be/swop/groep11/main/task/TaskExecuting.java
@@ -7,28 +7,47 @@ import java.time.LocalDateTime;
 
 
 /**
- * Created by warreee on 4/7/15.
+ * Stelt een taak status voor. Deze status is Executing.
  */
 public class TaskExecuting extends TaskStatus {
+    /**
+     * Maakt een nieuw TaskExecuting object aan.
+     */
     protected TaskExecuting() {
         super(TaskStatusEnum.EXECUTING);
     }
 
+    /**
+     * Haalt de status op.
+     */
     @Override
     public TaskStatusEnum getStatus() {
         return TaskStatusEnum.EXECUTING;
     }
 
+    /**
+     * Haalt een nieuwe TaskExecutingStatus op.
+     */
     @Override
     public TaskStatus getTaskStatus() {
         return new TaskExecuting();
     }
 
+    /**
+     * Start met de uitvoer van de gegeven taak op de gegeven moment. Dit mag niet dus gooit een exception.
+     * @param task De taak die moet uitgevoerd worden.
+     * @param startTime De starttijd van de uitvoer.
+     */
     @Override
     protected void execute(Task task, LocalDateTime startTime) {
         throw new IllegalStateTransitionException("De taak was reeds aan het uitvoeren!");
     }
 
+    /**
+     * Beeindigd de uitvoer van de gegeven taak op het gegeven moment.
+     * @param task De taak die moet beeindigd worden.
+     * @param endTime De eindtijd van de uitvoer.
+     */
     @Override
     protected void finish(Task task, LocalDateTime endTime) {
         task.setEndTime(endTime);
@@ -38,6 +57,11 @@ public class TaskExecuting extends TaskStatus {
         task.makeDependentTasksAvailable();
     }
 
+    /**
+     * Faalt de gegeven taak op het gegeven moment.
+     * @param task De taak die moet falen.
+     * @param endTime De tijd van het falen.
+     */
     @Override
     protected void fail(Task task, LocalDateTime endTime) {
         task.setEndTime(endTime);
