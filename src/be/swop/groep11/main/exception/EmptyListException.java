dee9fbc95dafad772e585d8da6b87590e35f1930
Robin Goots <robin5210@ulyssis.org>
2015-05-01 13:09:50 +0200

    Documentatie in EmptyListException.java

--- src/be/swop/groep11/main/exception/EmptyListException.java
+++ src/be/swop/groep11/main/exception/EmptyListException.java
@@ -1,7 +1,7 @@
 package be.swop.groep11.main.exception;
 
 /**
- * Created by Ronald on 10/03/2015.
+ * Exception die gegooid wordt wanneer er een lege lijst is.
  */
 public class EmptyListException extends RuntimeException {
     public EmptyListException(String message) {
