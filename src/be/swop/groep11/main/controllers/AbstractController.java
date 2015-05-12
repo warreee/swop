ad8e8613c110e67f433a8e23545cab212b775072
Ronald Spilstijns <sp.ronald.92@gmail.com>
2015-05-13 01:38:36 +0200

    AbstractController <-> ControllerStack update

--- src/be/swop/groep11/main/controllers/AbstractController.java
+++ src/be/swop/groep11/main/controllers/AbstractController.java
@@ -1,5 +1,6 @@
 package be.swop.groep11.main.controllers;
 
+import be.swop.groep11.main.actions.ControllerStack;
 import be.swop.groep11.main.ui.UserInterface;
 
 /**
@@ -7,6 +8,7 @@ import be.swop.groep11.main.ui.UserInterface;
  */
 public abstract class AbstractController {
     private final UserInterface userInterface;
+    private ControllerStack controllerStack;
 
     public AbstractController(UserInterface userInterface) {
         this.userInterface = userInterface;
@@ -50,5 +52,22 @@ public abstract class AbstractController {
 
     public void logon() throws IllegalArgumentException {
         throw new IllegalArgumentException("Niet ondersteund");
+    }
+
+    //TODO finish associatie AbstractController & ControllerStack Plus nieuwe Testen
+
+    public void setControllerStack(ControllerStack controllerStack)throws IllegalArgumentException {
+        if (!canHaveAsControllerStack(controllerStack)) {
+            throw new IllegalArgumentException("Ongeldige controllerStack voor deze AbstractController");
+        }
+        this.controllerStack = controllerStack;
+    }
+
+    public boolean canHaveAsControllerStack(ControllerStack controllerStack) {
+        return controllerStack != null && getControllerStack() == null;
+    }
+
+    public ControllerStack getControllerStack(){
+        return this.controllerStack;
     }
 }
