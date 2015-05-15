23d6d2b75c527cdf45171c0834a900480c394807
warreee <wardschodts@hotmail.com>
2015-05-15 11:20:56 +0200

    tot executing test herschreven in taskstatustest

--- tests/be/swop/groep11/test/unit/TaskStatusTest.java
+++ tests/be/swop/groep11/test/unit/TaskStatusTest.java
@@ -98,39 +98,39 @@ public class TaskStatusTest {
     @Test (expected = IllegalStateTransitionException.class)
     public void unavailableToExecuting() {
         task2.addNewDependencyConstraint(task1);
-        assertEquals(task1.getStatusString(), "AVAILABLE");
-        assertEquals(task2.getStatusString(), "UNAVAILABLE");
+        assertTrue(task1.isAvailable());
+        assertTrue(task2.isUnavailable());
         task2.execute(LocalDateTime.now());
     }
 
     @Test (expected = IllegalStateTransitionException.class)
     public void unavailableToFailed() {
         task2.addNewDependencyConstraint(task1);
-        assertEquals(task1.getStatusString(), "AVAILABLE");
-        assertEquals(task2.getStatusString(), "UNAVAILABLE");
+        assertTrue(task1.isAvailable());
+        assertTrue(task2.isUnavailable());
         task2.fail(LocalDateTime.now());
     }
 
     @Test (expected = IllegalStateTransitionException.class)
     public void unavailableToFinished() {
         task2.addNewDependencyConstraint(task1);
-        assertEquals(task1.getStatusString(), "AVAILABLE");
-        assertEquals(task2.getStatusString(), "UNAVAILABLE");
+        assertTrue(task1.isAvailable());
+        assertTrue(task2.isUnavailable());
         task2.finish(LocalDateTime.now());
     }
 
     @Test (expected = IllegalStateTransitionException.class)
     public void unavailableToUnavailable() throws Throwable {
         task2.addNewDependencyConstraint(task1);
-        assertEquals(task1.getStatusString(), "AVAILABLE");
-        assertEquals(task2.getStatusString(), "UNAVAILABLE");
+        assertTrue(task1.isAvailable());
+        assertTrue(task2.isUnavailable());
         TaskUnavailable test = (TaskUnavailable) task2.getStatus();
         try {
             TUmethodMakeUnAvailable.invoke(test, task2);
         } catch (InvocationTargetException e) {
             throw e.getTargetException();
         }
-        assertEquals(task2.getStatusString(), "UNAVAILABLE");
+        assertTrue(task2.isUnavailable());
     }
 
 
@@ -141,27 +141,27 @@ public class TaskStatusTest {
     @Test
     public void availableToUnavailable() {
         task2.addNewDependencyConstraint(task1);
-        assertEquals(task1.getStatusString(), "AVAILABLE");
-        assertEquals(task2.getStatusString(), "UNAVAILABLE");
+        assertTrue(task1.isAvailable());
+        assertTrue(task2.isUnavailable());
     }
 
     @Test
     public void availableToExecuting() throws Exception {
-        assertTrue(task1.getStatusString().equals("AVAILABLE"));
+        assertTrue(task1.isAvailable());
         task1.execute(LocalDateTime.of(2015, 3, 12, 8, 0));
-        assertTrue(task1.getStatusString().equals("EXECUTING"));
+        assertTrue(task1.isExecuting());
     }
 
     @Test(expected = IllegalStateTransitionException.class)
     public void availableToFailedTest() throws Exception {
-        assertTrue(task1.getStatusString().equals("AVAILABLE"));
+        assertTrue(task1.isAvailable());
         task1.fail(now);
     }
 
 
     @Test(expected = IllegalStateTransitionException.class)
     public void availableToFinishedTest() throws Exception {
-        assertTrue(task1.getStatusString().equals("AVAILABLE"));
+        assertTrue(task1.isAvailable());
         task1.finish(now);
     }
 
