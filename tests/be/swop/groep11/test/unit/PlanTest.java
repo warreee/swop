d8cb259a8b1df2036c3357b84f0411b2446a4410
warreee <wardschodts@hotmail.com>
2015-05-15 11:43:31 +0200

    uml aanpassing + plantest

---
+++ tests/be/swop/groep11/test/unit/PlanTest.java
@@ -0,0 +1,22 @@
+package be.swop.groep11.test.unit;
+
+import be.swop.groep11.main.planning.Plan;
+import be.swop.groep11.main.resource.ResourceManager;
+import org.junit.Before;
+
+import java.time.LocalDateTime;
+
+/**
+ * Testklasse voor de klasse Plan.
+ */
+public class PlanTest {
+    private Plan plan;
+    private ResourceManager resourceManager;
+    private LocalDateTime now;
+
+    @Before
+    public void setUp {
+        now =
+    }
+
+}
