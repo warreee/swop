1560b293930f3cf3ae690bed3fc8c85c68d9f58d
Ward Schodts <wardschodts@hotmail.com>
2015-05-15 20:57:35 +0200

    projectmanager constructor

--- src/be/swop/groep11/main/resource/ProjectManager.java
+++ src/be/swop/groep11/main/resource/ProjectManager.java
@@ -1,9 +1,19 @@
 package be.swop.groep11.main.resource;
 
+import be.swop.groep11.main.core.User;
+
 /**
  * Klasse voor het voorstellen van de user projectmanager
  */
-public class ProjectManager {
+public class ProjectManager extends User{
 
 
+    /**
+     * Initialiseerd deze user met de gegeven naam.
+     *
+     * @param name De naam van deze user.
+     */
+    public ProjectManager(String name) {
+        super(name);
+    }
 }
