7b4e67c65edeb32f52c2853a1bf9471f998be47e
Ward Schodts <wardschodts@hotmail.com>
2015-05-16 11:31:17 +0200

    logon setup

--- tests/be/swop/groep11/test/integration/LogonScenarioTest.java
+++ tests/be/swop/groep11/test/integration/LogonScenarioTest.java
@@ -1,9 +1,10 @@
 package be.swop.groep11.test.integration;
 
 import be.swop.groep11.main.core.BranchOffice;
+import be.swop.groep11.main.core.Company;
 import be.swop.groep11.main.core.ProjectRepository;
 import be.swop.groep11.main.core.SystemTime;
-import be.swop.groep11.main.resource.ResourcePlanner;
+import be.swop.groep11.main.resource.*;
 import be.swop.groep11.main.ui.UserInterface;
 import org.junit.Before;
 import org.junit.Test;
@@ -12,6 +13,7 @@ import java.time.LocalDateTime;
 
 import static org.junit.Assert.fail;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 /**
  * Testklasse om de usecase logon te testen.
@@ -23,17 +25,26 @@ public class LogonScenarioTest {
 
     @Before
     public void setUp() throws Exception {
+        Company company = new Company("company", mock(ResourceTypeRepository.class));
         this.mockedUI = mock(UserInterface.class);
         ProjectRepository projectRepository = mock(ProjectRepository.class);
         ResourcePlanner resourcePlanner = mock(ResourcePlanner.class);
         this.branchOffice = new BranchOffice("Branchoffice 1", "Leuven", projectRepository, resourcePlanner);
-
-
+        Developer developer1 = new Developer("dev1", mock(ResourceType.class)); // TODO: moet er echt telkends een resourcetype worden meegegeven
+        Developer developer2 = new Developer("dev2", mock(ResourceType.class)); // TODO: kunnen we geen standaard constructor in devloper aanmaken?
+        ProjectManager projectManager = new ProjectManager("projectmanager1");
+        branchOffice.addEmployee(developer1);
+        branchOffice.addEmployee(developer2);
+        branchOffice.addEmployee(projectManager);
     }
 
+    /**
+     *
+     * @throws Exception
+     */
     @Test
-    public void test() throws Exception {
+    public void logonValidTest() throws Exception {
         fail("implement tests");
-
+        //when(mockedUI.selectBranchOfficeFromList()
     }
 }
