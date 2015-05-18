31de125537e077f86c923305eb6b36511ba6117c
warreee <wardschodts@hotmail.com>
2015-05-18 18:51:32 +0200

    delegateTask test1

--- tests/be/swop/groep11/test/integration/DelegateTaskScenarioTest.java
+++ tests/be/swop/groep11/test/integration/DelegateTaskScenarioTest.java
@@ -1,12 +1,16 @@
 package be.swop.groep11.test.integration;
 
 import be.swop.groep11.main.controllers.DelegateTaskController;
+import be.swop.groep11.main.controllers.LogonController;
 import be.swop.groep11.main.core.BranchOffice;
 import be.swop.groep11.main.core.Company;
+import be.swop.groep11.main.task.Task;
 import be.swop.groep11.main.ui.UserInterface;
+import com.google.common.collect.ImmutableList;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -21,6 +25,10 @@ public class DelegateTaskScenarioTest {
     private Company company;
 
     private DelegateTaskController delegateTaskController;
+    private ImmutableList<BranchOffice> branchOffices;
+    private ImmutableList<Task> unplannedTasks;
+
+    private LogonController logonController;
 
     @Before
     public void setUp() throws Exception {
@@ -31,10 +39,16 @@ public class DelegateTaskScenarioTest {
     /**
      * Test voor het tonen van de ongeplande taken in de ingelogde branchoffice.
      * Hierna wordt er 1 taak geselecteerd.
+     * Test voor het tonen van de branchoffices. Hierna wordt er 1 geselecteerd.
      */
     @Test
     public void delegateTaskShowUnplannedTasksTest() {
-        fail("Nog niet geimplementeerd.");
+        when(mockedUI.selectTaskFromList(unplannedTasks)).thenReturn(unplannedTasks.get(0));
+        when(mockedUI.selectBranchOfficeFromList(branchOffices)).thenReturn(branchOffices.get(0));
+
+        delegateTaskController.delegateTask();
+
+        assertTrue(branchOffices.get(0).getDelegatedTasks().contains(unplannedTasks.get(0)));
     }
 
     /**
