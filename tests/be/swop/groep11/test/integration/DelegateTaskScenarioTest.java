73b2783a2b1f293284da22009d3d71fccb42448d
warreee <wardschodts@hotmail.com>
2015-05-18 16:42:51 +0200

    TODOs weg weg :D

--- tests/be/swop/groep11/test/integration/DelegateTaskScenarioTest.java
+++ tests/be/swop/groep11/test/integration/DelegateTaskScenarioTest.java
@@ -1,8 +1,24 @@
 package be.swop.groep11.test.integration;
 
+import be.swop.groep11.main.controllers.DelegateTaskController;
+import be.swop.groep11.main.core.BranchOffice;
+import be.swop.groep11.main.core.Company;
+import be.swop.groep11.main.ui.UserInterface;
+import org.junit.Before;
+
 /**
  * Test om de usecase delegate task scenario na te gaan.
  */
 public class DelegateTaskScenarioTest {
 
+    private UserInterface mockedUI;
+    private BranchOffice branchOffice;
+    private Company company;
+
+    private DelegateTaskController delegateTaskController;
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
 }
