e4c347d16a6022150f9aefd7aa7492411964c791
warreee <wardschodts@hotmail.com>
2015-05-18 17:57:37 +0200

    selectDelegateTask

--- src/be/swop/groep11/main/controllers/DelegateTaskController.java
+++ src/be/swop/groep11/main/controllers/DelegateTaskController.java
@@ -1,7 +1,12 @@
 package be.swop.groep11.main.controllers;
 
 import be.swop.groep11.main.core.Company;
+import be.swop.groep11.main.exception.CancelException;
+import be.swop.groep11.main.exception.EmptyListException;
+import be.swop.groep11.main.exception.InterruptedAProcedureException;
+import be.swop.groep11.main.task.Task;
 import be.swop.groep11.main.ui.UserInterface;
+import com.google.common.collect.ImmutableList;
 
 /**
  * Klasse voor de controller van de usecase delegatetask.
@@ -19,6 +24,28 @@ public class DelegateTaskController  extends AbstractController {
 
     @Override
     public void delegateTask() {
-        System.out.println("nog te implementeren");
+        try {
+            selectDelegateTask();
+            selectDestinationBranchoffice();
+        } catch (CancelException | EmptyListException e) {
+            getUserInterface().printException(e);
+            throw new InterruptedAProcedureException(); // zorgt ervoor dat de status van de stack hersteld wordt
+        }
     }
+
+    Task delegatedTask;
+
+    private void setDelegateTask(Task delegatedTask) {
+        this.delegatedTask = delegatedTask;
+    }
+
+    private void selectDelegateTask() {
+        ImmutableList<Task> tasks = ImmutableList.copyOf(logonController.getBranchOffice().getUnplannedTasks());
+        setDelegateTask(getUserInterface().selectTaskFromList(tasks));
+    }
+
+    private void selectDestinationBranchoffice() {
+
+    }
+
 }
--- tests/be/swop/groep11/test/integration/DelegateTaskScenarioTest.java
+++ tests/be/swop/groep11/test/integration/DelegateTaskScenarioTest.java
@@ -5,6 +5,11 @@ import be.swop.groep11.main.core.BranchOffice;
 import be.swop.groep11.main.core.Company;
 import be.swop.groep11.main.ui.UserInterface;
 import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.fail;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 /**
  * Test om de usecase delegate task scenario na te gaan.
@@ -20,5 +25,33 @@ public class DelegateTaskScenarioTest {
     @Before
     public void setUp() throws Exception {
 
+        this.mockedUI = mock(UserInterface.class);
     }
+
+    /**
+     * Test voor het tonen van de ongeplande taken in de ingelogde branchoffice.
+     * Hierna wordt er 1 taak geselecteerd.
+     */
+    @Test
+    public void delegateTaskShowUnplannedTasksTest() {
+        fail("Nog niet geimplementeerd.");
+    }
+
+    /**
+     * Test voor het tonen van de branchoffices. Hierna wordt er 1 geselecteerd.
+     */
+    @Test
+    public void delegateTaskShowBranchOfficesTest() {
+        fail("Nog niet geimplementeerd.");
+    }
+
+    /**
+     * Test om het volledige scenario te testen.
+     */
+    @Test
+    public void delegateTaskTest() {
+        fail("Nog niet geimplementeerd.");
+    }
+
+
 }
