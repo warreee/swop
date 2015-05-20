7d47b4cd7adeae600eb1001af02f1c1c8cd68f34
Robin Goots <robin5210@ulyssis.org>
2015-05-20 10:08:21 +0200

    Al een hele tijd niet meer gepushed.

--- src/be/swop/groep11/main/util/InputParser.java
+++ src/be/swop/groep11/main/util/InputParser.java
@@ -1,6 +1,8 @@
 package be.swop.groep11.main.util;
 
 import be.swop.groep11.main.core.*;
+import be.swop.groep11.main.planning.Plan;
+import be.swop.groep11.main.planning.PlanBuilder;
 import be.swop.groep11.main.resource.*;
 import be.swop.groep11.main.task.Task;
 import org.yaml.snakeyaml.Yaml;
@@ -15,6 +17,8 @@ import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 import static org.mockito.Mockito.mock;
 
@@ -23,6 +27,7 @@ import static org.mockito.Mockito.mock;
  */
 public class InputParser {
     private ProjectRepository projectRepository;
+    private ArrayList<BranchOffice> branchOfficeList = new ArrayList<>();
     private ArrayList<Project> projectList = new ArrayList<>();
     private ArrayList<Task> taskList = new ArrayList<>();
     private ArrayList<AResourceType> resourceTypeList = new ArrayList<>();
@@ -32,23 +37,26 @@ public class InputParser {
     private ArrayList<Map<String, Object>> planningsList = new ArrayList<>();
     private Map<Integer, Map<Integer, Map<String, LocalDateTime>>> reservationsMap = new HashMap<>();
     private SystemTime systemTime;
+    private Company company;
+    private ResourceTypeRepository resourceTypeRepository;
 
     /**
      * Initialiseerd deze inputparser.
      * @param projectRepository De ProjectRepository waaraan alle projecten moeten worden toegevoegd.
      */
-    public InputParser(ProjectRepository projectRepository , SystemTime systemTime) {
+    public InputParser(ProjectRepository projectRepository, SystemTime systemTime, ResourceTypeRepository resourceTypeRepository) {
         this.projectRepository = projectRepository;
         this.systemTime = systemTime;
+        this.resourceTypeRepository = resourceTypeRepository;
     }
 
 
     public static void main(String[] args) throws FileNotFoundException {
         SystemTime systemTime = new SystemTime(LocalDateTime.MIN);
-        BranchOffice branchOffice = mock(BranchOffice.class); // TODO: echte branchoffice erinsteken!
         ProjectRepository projectRepository = new ProjectRepository(systemTime);
+        ResourceTypeRepository resourceTypeRepository = new ResourceTypeRepository();
 
-        InputParser parser = new InputParser(projectRepository, systemTime);
+        InputParser parser = new InputParser(projectRepository, systemTime, resourceTypeRepository);
         parser.parseInputFile();
         System.out.println("Finished :)");
     }
@@ -61,7 +69,7 @@ public class InputParser {
     public void parseInputFile() throws FileNotFoundException {
 
         Yaml yaml = new Yaml();
-        String path = Paths.get("input/input.tman").toAbsolutePath().toString();
+        String path = Paths.get("input/input_1.yaml").toAbsolutePath().toString();
 
         Map<String, Object> values;
 
@@ -73,21 +81,29 @@ public class InputParser {
         }
 
         // De volgorde is van belang.
-        handleDevelopers(values);
-        handleDailyAvailability(values);
         handleSystemTime(values);
         handleCompany(values);
-        handleReservations(values);
-        handlePlannings(values);
+        handleBranchOffiches(values);
+        handleDevelopers(values);
+        handleProjectManagers(values);
+        handleDailyAvailability(values);
         handleResourceTypes(values);
         handleResources(values);
         handleProjects(values);
         handleTasks(values);
+        handlePlans(values);
     }
 
-    private void handleCompany(Map<String, Object> companyMap){
+    private void handleBranchOffiches(Map<String, Object> branchOfficeMap){
+        ArrayList branchOffiches = (ArrayList) branchOfficeMap.get("branchOffice");
+        for(Object branchOffice: branchOffiches){
+            addBranchOffice((Map<String, String>) branchOffice);
+        }
+    }
+
+    public void handleCompany(Map<String, Object> companyMap){
         String name = (String) companyMap.get("company");
-        // TODO: in company object steken.
+        company = new Company(name, new ResourceTypeRepository(), getSystemTime());
     }
 
     /**
@@ -109,7 +125,7 @@ public class InputParser {
     private void handleDevelopers(Map<String, Object> developersMap){
         ArrayList developers = (ArrayList) developersMap.get("developers");
         for(Object developer: developers){
-            addDeveloper((Map<String, String>) developer);
+            addDeveloper((Map<String, Object>) developer);
         }
     }
 
@@ -119,22 +135,15 @@ public class InputParser {
      * beschikbaar).
      * @param planningsMap
      */
-    private void handlePlannings(Map<String, Object> planningsMap){
-        ArrayList plannings = (ArrayList) planningsMap.get("plannings");
-        for (int i = 0; i < plannings.size(); i++){
-            planningsList.add(new HashMap<>());
-            Map<String, Object> planningMap = (Map<String, Object>) plannings.get(i);
-            Map<String, Object> planningListMap = planningsList.get(planningsList.size() - 1);
-
-            planningListMap.put("plannedStartTime", parseTime((String) planningMap.get("plannedStartTime")));
-            planningListMap.put("developers", planningMap.get("developers"));
-            HashMap<Integer, Integer> resourcesMap = new HashMap<>();
-            planningListMap.put("resources", resourcesMap);
-            for(Map<Integer, Integer> rMap: (ArrayList<Map<Integer, Integer>>) planningMap.get("resources")){
-                resourcesMap.put(rMap.get("type"), rMap.get("quantity"));
-            }
+    private void handlePlans(Map<String, Object> planningsMap){
+        ArrayList<Map<String, Object>> plannings = (ArrayList) planningsMap.get("plans");
+        for(Map<String, Object> plan: plannings){
+            addPlan(plan);
         }
-        return;
+    }
+
+    private void handleProjectManagers(Map<String, Object> projectManagersMap){
+        // TODO
     }
 
     /**
@@ -144,26 +153,8 @@ public class InputParser {
     private void handleProjects(Map<String, Object> projectsMap){
         ArrayList projects = (ArrayList) projectsMap.get("projects");
         for (Object project : projects) {
-            addProject((Map<String, String>) project);
+            addProject((Map<String, Object>) project);
         }
-    }
-
-    /**
-     * Leest alle reservaties in.
-     * @param reservationsMap
-     */
-    private void handleReservations(Map<String, Object> reservationsMap){
-        ArrayList reservations = (ArrayList) reservationsMap.get("reservations");
-        for(Object reservation: reservations){
-            Map<String, Object> reservationMap = (Map<String, Object>) reservation;
-            Integer taskId = (Integer) reservationMap.get("task");
-            Integer resourceID = (Integer) reservationMap.get("resource");
-            this.reservationsMap.putIfAbsent(taskId, new HashMap<>());
-            this.reservationsMap.get(taskId).putIfAbsent(resourceID, new HashMap<>());
-            this.reservationsMap.get(taskId).get(resourceID).put("startTime", parseTime((String) reservationMap.get("startTime")));
-            this.reservationsMap.get(taskId).get(resourceID).put("endTime", parseTime((String) reservationMap.get("endTime")));
-        }
-        return;
     }
 
     /**
@@ -184,7 +175,7 @@ public class InputParser {
     private void handleResourceTypes(Map<String, Object> resourceTypeMap){
         ArrayList resourceTypes = (ArrayList) resourceTypeMap.get("resourceTypes");
         for(int i = 0; i < resourceTypes.size(); i++){
-            addResourceType(i, (Map<String, Object>) resourceTypes.get(i));
+            addResourceType((Map<String, Object>) resourceTypes.get(i));
         }
     }
 
@@ -210,6 +201,15 @@ public class InputParser {
         }
     }
 
+    private void addBranchOffice(Map<String, String> propertiesList){
+        String name = propertiesList.get("name");
+        String location = propertiesList.get("location");
+        ResourcePlanner planner = new ResourcePlanner(new ResourceRepository(resourceTypeRepository), getSystemTime());
+        BranchOffice branchOffice = new BranchOffice(name, location, new ProjectRepository(getSystemTime()), planner); // TODO: juiste objecten meegeven
+        branchOfficeList.add(branchOffice);
+        company.addBranchOffice(branchOffice);
+    }
+
     /**
      * Maakt een nieuwe DailyAvailability aan. Deze wordt bijgehouden in een interne lijst om te gebruiken bij het
      * toevoegen van nieuwe ResourceTypes aan ResourceTypeRepository.
@@ -226,13 +226,33 @@ public class InputParser {
      * Voegt een nieuwe Developer toe aan de gegeven ResourceManager.
      * @param propertiesList
      */
-    private void addDeveloper(Map<String, String> propertiesList){
-        /* TODO dit werkt niet meer, maar de input parser moet toch aangepast worden...
-        String name = propertiesList.get("name");
-        AResourceType resourceType = resourceManager.getDeveloperType();
-        resourceManager.addResourceInstance(resourceType, name);
-        developerList.add(resourceManager.getDeveloperType().getResourceInstances().get(resourceManager.getDeveloperType().getResourceInstances().size() - 1));
-        */
+    private void addDeveloper(Map<String, Object> propertiesList){
+        String name = (String) propertiesList.get("name");
+        int branch = (int) propertiesList.get("branchOffice");
+        Developer developer = new Developer(name);
+        branchOfficeList.get(branch).addEmployee(developer);
+        developerList.add(developer);
+    }
+
+    private void addPlan(Map<String, Object> propertiesList){
+        BranchOffice branchOffice = branchOfficeList.get((Integer) propertiesList.get("branchOffice"));
+        Task task = taskList.get((Integer) propertiesList.get("task"));
+        List<ResourceInstance> specific = ((List<Integer>) propertiesList.get("specificResources")).stream()
+                .map(resourceInstanceList::get).collect(Collectors.toList());
+        List<ResourceInstance> nonSpecific = ((List<Integer>) propertiesList.get("nonSpecificResources")).stream()
+                .map(resourceInstanceList::get).collect(Collectors.toList());
+        List<ResourceInstance> developers = ((List<Integer>) propertiesList.get("developers")).stream()
+                .map(developerList::get).collect(Collectors.toList());
+        LocalDateTime startTime = parseTime((String) propertiesList.get("startTime"));
+        LocalDateTime endTime = parseTime((String) propertiesList.get("endTime"));
+
+        PlanBuilder builder = new PlanBuilder(branchOffice, task, startTime);
+        specific.forEach(builder::addResourceInstance);
+        developers.forEach(builder::addResourceInstance);
+
+        Plan plan = builder.getPlan(endTime, nonSpecific);
+        branchOffice.getResourcePlanner().addPlan(plan);
+        // TODO: Is alles hiervan klaar?
     }
 
     /**
@@ -240,17 +260,25 @@ public class InputParser {
      *
      * @param propertiesList de eigenschappen van het project ingelezen vanuit de tman file.
      */
-    private void addProject(Map<String, String> propertiesList) {
+    private void addProject(Map<String, Object> propertiesList) {
+        String name = (String) propertiesList.get("name");
+        String description = (String) propertiesList.get("description");
+        LocalDateTime creationTime = parseTime((String) propertiesList.get("creationTime"));
+        LocalDateTime dueTime = parseTime((String) propertiesList.get("dueTime"));
+        int branch = (int) propertiesList.get("branchOffice");
 
-        String name = propertiesList.get("name");
-        String description = propertiesList.get("description");
-        LocalDateTime creationTime = parseTime(propertiesList.get("creationTime"));
-        LocalDateTime dueTime = parseTime(propertiesList.get("dueTime"));
-
-        projectRepository.addNewProject(name, description, creationTime, dueTime);
+        ProjectRepository repo = branchOfficeList.get(branch).getProjectRepository();
+        repo.addNewProject(name, description, creationTime, dueTime);
 
         // Haalt het laatst toegevoegde project op.
-        projectList.add(projectRepository.getProjects().get(projectRepository.getProjects().size() - 1));
+        projectList.add(repo.getProjects().get(repo.getProjects().size() - 1));
+    }
+
+    private void addProjectManager(Map<String, Object> propertiesList){
+        String name = (String) propertiesList.get("name");
+        int branch = (int) propertiesList.get("branchOffice");
+        ProjectManager manager = new ProjectManager(name);
+        branchOfficeList.get(branch).addEmployee(manager);
     }
 
     /**
@@ -258,53 +286,35 @@ public class InputParser {
      * @param propertiesList
      */
     private void addResource(Map<String, Object> propertiesList){
-//        String name = (String) propertiesList.get("name");
-//        AResourceType resourceType = resourceTypeList.get((Integer) propertiesList.get("type"));
-//        resourceManager.addResourceInstance(resourceType, name);
-//        int size = resourceType.getResourceInstances().size();
-//        resourceInstanceList.add(resourceType.getResourceInstances().get(size - 1));
+        String name = (String) propertiesList.get("name");
+        AResourceType resourceType = resourceTypeList.get((Integer) propertiesList.get("type"));
+        BranchOffice branchOffice = branchOfficeList.get((int) propertiesList.get("branchOffice"));
+        Resource resource = new Resource(name, resourceType);
+        branchOffice.getResourceRepository().addResourceInstance(resource);
+        resourceInstanceList.add(resource);
     }
 
     /**
      * Voegt een nieuw ResourceType toe.
      * @param propertiesList
      */
-    private void addResourceType(int number, Map<String, Object> propertiesList){
-        /* TODO dit werkt niet meer, maar de input parser moet toch aangepast worden...
+    private void addResourceType(Map<String, Object> propertiesList){
         // Lees alle info uit de map.
         String name = (String) propertiesList.get("name");
         ArrayList requires = (ArrayList) propertiesList.get("requires");
         ArrayList conflictsWith = (ArrayList) propertiesList.get("conflictsWith");
         Integer dailyAvailability = (Integer) propertiesList.get("dailyAvailability");
 
-        // Haal de correcte IResourceTypes op die al bekend zijn. (Dit faalt indien er fouten staan in de inputfile.)
-        ArrayList<AResourceType> req = new ArrayList<>();
-        ArrayList<AResourceType> con = new ArrayList<>();
-        requires.forEach(x -> req.add(resourceTypeList.get((Integer) x)));
-        conflictsWith.forEach(x -> {
-            if ((Integer) x != number) {
-                con.add(resourceTypeList.get((Integer) x));
-            }
-        });
-
-        // Voeg de IResourceType eindelijk toe.
-        if(dailyAvailability == null){
-            // Er is geen dailyAvailability.
-            resourceManager.addNewResourceType(name, req, con);
+        if(dailyAvailability != null){
+            resourceTypeRepository.addNewResourceType(name,dailyAvailabilityList.get(dailyAvailability));
         } else {
-            // Er is wel een dailyAvailability.
-            DailyAvailability da = dailyAvailabilityList.get(Integer.valueOf(dailyAvailability));
-            resourceManager.addNewResourceType(name, da, req, con);
+            resourceTypeRepository.addNewResourceType(name);
         }
+        AResourceType type = resourceTypeRepository.getResourceTypeByName(name);
+        resourceTypeList.add(type);
 
-        // Indien het IResourceType conflicteerd met zichzelf, voeg dit dan toe.
-        AResourceType type = resourceManager.getResourceTypeByName(name);
-        if(conflictsWith.contains(number)){
-            resourceManager.withConflictConstraint(type, type);
-        }
-
-        resourceTypeList.add(resourceManager.getResourceTypeByName(name));
-        */
+        requires.forEach(x -> resourceTypeRepository.withRequirementConstraint(type, resourceTypeList.get((Integer) x)));
+        conflictsWith.forEach(x -> resourceTypeRepository.withConflictConstraint(type, resourceTypeList.get((Integer) x)));
     }
 
     /**
@@ -319,11 +329,41 @@ public class InputParser {
         Duration duration = Duration.ofMinutes(Long.valueOf(String.valueOf(propertiesList.get("estimatedDuration"))));
         Double acceptableDeviation = Double.valueOf(String.valueOf(propertiesList.get("acceptableDeviation"))) / 100;
         Project project = projectList.get((Integer) propertiesList.get("project"));
-        //RequirementListBuilder requirementListBuilder = new RequirementListBuilder();
-        //requirementListBuilder.addNewRequirement(resourceManager.getDeveloperType(), 1);
-        //project.addNewTask(description, acceptableDeviation, duration, requirementListBuilder.getRequirements());
+        Integer alternative = (Integer) propertiesList.get("alternativeFor");
+        ArrayList<Integer> prerequisiteTasks = (ArrayList<Integer>) propertiesList.get("prerequisiteTasks");
+        String status = (String) propertiesList.get("status");
+        ArrayList<Map<String, Integer>> resourceRequirement = (ArrayList<Map<String, Integer>>) propertiesList.get("resourceRequirements");
+        Integer numberDevelopers = (Integer) propertiesList.get("numberDevelopers");
+        Integer delegatedTo = (Integer) propertiesList.get("delegatedTo");
+
+        // Maak een RequirementListBuilder en voeg alle requirements daar aan toe.
+        RequirementListBuilder builder = new RequirementListBuilder(project.getBranchOffice().getResourceRepository());
+        //builder.addNewRequirement(resourceTypeRepository.getDeveloperType(), numberDevelopers); TODO: terug aanzetten wanneer dit terug werkt.
+        for(Map<String, Integer> requirement: resourceRequirement){
+            AResourceType type = resourceTypeList.get(requirement.get("type"));
+            Integer amount = requirement.get("amount");
+            builder.addNewRequirement(type, amount);
+        }
+
+        project.addNewTask(description, acceptableDeviation, duration, builder.getRequirements());
         taskList.add(project.getTasks().get(project.getTasks().size() - 1));
         Task task = taskList.get(taskList.size() - 1);
+
+        // Voeg alle taken toe op welke deze taak depend.
+        prerequisiteTasks.forEach(x -> task.addNewDependencyConstraint(taskList.get(x)));
+
+        // Indien dit een alternatieve taak is voor een andere taak. Zet dan deze taak als alternatief.
+        if(alternative != null){
+            taskList.get(alternative).setAlternativeTask(task);
+        }
+
+        // Indien deze taak geledegeerd is naar een andere branchOffice, delegeer dan deze taak.
+        if(delegatedTo != null){
+            project.getBranchOffice().delegateTask(task, branchOfficeList.get(delegatedTo));
+        }
+
+        // TODO: afwerken
+
         ArrayList<Developer> taskDevelopers = new ArrayList<>();
         LocalDateTime plannedStartTime = null;
         /*
@@ -402,6 +442,10 @@ public class InputParser {
      */
     public SystemTime getSystemTime(){
         return this.systemTime;
+    }
+
+    public Company getCompany(){
+        return this.company;
     }
 
     /**
