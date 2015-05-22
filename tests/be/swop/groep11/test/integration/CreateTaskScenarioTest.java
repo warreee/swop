44895cd5244097d4b53c0b2d2461fabfbb231cc5
Arne De Brabandere <arne.de.brabandere@telenet.be>
2015-05-22 11:33:20 +0200

    Create task scenario test

--- tests/be/swop/groep11/test/integration/CreateTaskScenarioTest.java
+++ tests/be/swop/groep11/test/integration/CreateTaskScenarioTest.java
@@ -1,15 +1,13 @@
 package be.swop.groep11.test.integration;
 
 import be.swop.groep11.main.controllers.LogonController;
+import be.swop.groep11.main.controllers.SimulationController;
 import be.swop.groep11.main.controllers.TaskController;
-import be.swop.groep11.main.core.BranchOffice;
-import be.swop.groep11.main.core.Project;
-import be.swop.groep11.main.core.ProjectRepository;
-import be.swop.groep11.main.core.SystemTime;
+import be.swop.groep11.main.core.*;
 import be.swop.groep11.main.exception.CancelException;
-import be.swop.groep11.main.resource.DeveloperType;
-import be.swop.groep11.main.resource.ResourcePlanner;
-import be.swop.groep11.main.resource.ResourceRepository;
+import be.swop.groep11.main.planning.Plan;
+import be.swop.groep11.main.planning.PlanBuilder;
+import be.swop.groep11.main.resource.*;
 import be.swop.groep11.main.resource.constraint.ResourceTypeConstraint;
 import be.swop.groep11.main.task.Task;
 import be.swop.groep11.main.ui.UserInterface;
@@ -17,7 +15,9 @@ import com.google.common.collect.ImmutableList;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.time.Duration;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -30,50 +30,88 @@ import static org.mockito.Mockito.*;
  */
 public class CreateTaskScenarioTest {
     private LocalDateTime now;
-    private ProjectRepository projectRepository;
-    private TaskController taskController;
     private UserInterface mockedUI;
     private SystemTime systemTime;
-    private ImmutableList<Project> projects;
-    private List<Task> tasks;
-    private LogonController logonController;
+    private TaskController taskController;
+    private BranchOffice branchOffice, branchOffice2;
+    private ProjectRepository projectRepository;
+    private List<Project> projects;
+    private ResourcePlanner resourcePlanner;
+    private Developer devA, devB;
+    private Task taskA, taskB;
 
 
     @Before
     public void setUp() throws Exception {
-        now = LocalDateTime.now();
+        now = LocalDateTime.of(2015, 1, 1, 11, 0);
         this.mockedUI = mock(UserInterface.class);
 
         systemTime = new SystemTime(now);
-        BranchOffice branchOffice = mock(BranchOffice.class);
-        projectRepository = new ProjectRepository(systemTime);
-        projectRepository.setBranchOffice(branchOffice);
+
         addTempDomainObjects();
 
-        projectRepository.addNewProject("Naam1", "Omschrijving1", LocalDateTime.now(), now.plusDays(10));
-
-        this.logonController = mock(LogonController.class);
+        LogonController logonController = mock(LogonController.class);
         when(logonController.hasIdentifiedProjectManager()).thenReturn(true);
+        when(logonController.getBranchOffice()).thenReturn(branchOffice);
+        when(logonController.getProjectManager()).thenReturn(new ProjectManager("PM"));
 
         this.taskController = new TaskController(logonController,mockedUI);
-
-        this.projects = projectRepository.getProjects();
-        this.tasks = projects.get(0).getTasks();
-
-        ResourceRepository resourceRepository = mock(ResourceRepository.class);
-
-        when(logonController.getBranchOffice()).thenReturn(branchOffice);
-        when(branchOffice.getProjectRepository()).thenReturn(projectRepository);
-        ResourcePlanner resourcePlanner = new ResourcePlanner(resourceRepository, systemTime);
-        when(branchOffice.getResourcePlanner()).thenReturn(resourcePlanner);
-        when(branchOffice.getResourceRepository()).thenReturn(resourceRepository);
-        when(resourceRepository.getPresentResourceTypes()).thenReturn(ImmutableList.copyOf(new ArrayList<>()));
-
-        DeveloperType developerType = mock(DeveloperType.class);
-        when(resourceRepository.getDeveloperType()).thenReturn(developerType);
+    }
 
 
-        //when(developerType.getConstraintFor(developerType).isAcceptableAmount(any(), anyInt())).thenReturn(true);
+    private void addTempDomainObjects() {
+        ResourceTypeRepository resourceTypeRepository = new ResourceTypeRepository();
+
+        projectRepository = new ProjectRepository(systemTime);
+        ResourceRepository resourceRepository = new ResourceRepository(resourceTypeRepository);
+        resourcePlanner = new ResourcePlanner(resourceRepository, systemTime);
+        branchOffice = new BranchOffice("Branch Office 1", "Leuven", projectRepository, resourcePlanner);
+        branchOffice2 = new BranchOfficeProxy(new BranchOffice("Branch Office 2", "Mechelen", new ProjectRepository(systemTime), resourcePlanner));
+
+        AResourceType devType = resourceTypeRepository.getDeveloperType();
+        devA = new Developer("DevA", devType);
+        devB = new Developer("DevB", devType);
+
+        branchOffice.addEmployee(new Developer("DevA", devType));
+        branchOffice.addEmployee(new Developer("DevB", devType));
+        branchOffice.addEmployee(new ProjectManager("PM1"));
+
+        resourceTypeRepository.addNewResourceType("Auto");
+        AResourceType autoType = resourceTypeRepository.getResourceTypeByName("Auto");
+
+        resourceRepository.addResourceInstance(new Resource("Aston Martin Rapide", autoType));
+        resourceRepository.addResourceInstance(new Resource("Toyota Auris", autoType));
+        resourceRepository.addResourceInstance(new Resource("Rolls Royce Phantom", autoType));
+
+        resourceTypeRepository.addNewResourceType("CarWash", new DailyAvailability(LocalTime.of(10, 0), LocalTime.of(14, 0)));
+        AResourceType carWashType = resourceTypeRepository.getResourceTypeByName("CarWash");
+
+        resourceRepository.addResourceInstance(new Resource("CarWash A", carWashType));
+        resourceRepository.addResourceInstance(new Resource("CarWash B", carWashType));
+        resourceRepository.addResourceInstance(new Resource("CarWash C", carWashType));
+
+
+        RequirementListBuilder requirementListBuilderA = new RequirementListBuilder(resourceRepository);
+        requirementListBuilderA.addNewRequirement(autoType, 2);
+        requirementListBuilderA.addNewRequirement(carWashType, 1);
+        requirementListBuilderA.addNewRequirement(devType, 1);
+
+        RequirementListBuilder requirementListBuilderB = new RequirementListBuilder(resourceRepository);
+        requirementListBuilderB.addNewRequirement(carWashType, 2);
+        requirementListBuilderB.addNewRequirement(devType, 1);
+
+        projectRepository.addNewProject("Project 11", "Omschrijving1", now, now.plusDays(10));
+        projectRepository.getProjects().get(0).addNewTask("Taak 1", 1, Duration.ofMinutes(120), requirementListBuilderA.getRequirements());
+        taskA = projectRepository.getProjects().get(0).getLastAddedTask();
+        projectRepository.getProjects().get(0).addNewTask("Taak 2", 1, Duration.ofMinutes(120), requirementListBuilderB.getRequirements());
+        taskB = projectRepository.getProjects().get(0).getLastAddedTask();
+        projects = projectRepository.getProjects();
+
+        PlanBuilder planBuilder = new PlanBuilder(branchOffice, taskB, now);
+        planBuilder.proposeResources();
+        planBuilder.addResourceInstance(devA);
+        Plan plan = planBuilder.getPlan();
+        resourcePlanner.addPlan(plan);
     }
 
     @Test
@@ -163,7 +201,7 @@ public class CreateTaskScenarioTest {
         taskController.createTask();
     }
 
-    private void addTempDomainObjects() {
+   // private void addTempDomainObjects() {
 
 //        resourceManager.addResourceInstance(resourceManager.getDeveloperType(), "Kabouter SWOP");
 //        resourceManager.addResourceInstance(resourceManager.getDeveloperType(), "Kabouter Ward");
@@ -180,6 +218,6 @@ public class CreateTaskScenarioTest {
 //        resourceManager.addResourceInstance(resourceManager.getResourceTypeByName("Koets"), "Koets 1");
 //        resourceManager.addResourceInstance(resourceManager.getResourceTypeByName("Koets"), "Koets 2");
 
-    }
+   // }
 
 }
